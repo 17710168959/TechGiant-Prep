87aeddf7899591c6c3f32822deaca978e6af39c6
Your Name <you@example.com>
2025-06-05 22:11:39 +0800

    111

---
+++ consolidation/src/test/java/com/zyd/consolidation/ConsolidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyd.consolidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsolidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
